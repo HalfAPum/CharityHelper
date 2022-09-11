473f3cd5df8d6492c1e4564ad997207ce94f9b6e
o.narvatov <oleksandr.narvatov@playtech.com>
2022-09-11 18:05:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/planthelper/ui/PlantApplication.kt
@@ -0,0 +1,11 @@
+package com.example.planthelper.ui
+
+import android.app.Application
+
+class PlantApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
